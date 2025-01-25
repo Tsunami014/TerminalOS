f5e5af8bac60bdaf9e2f9819a5d8b09ab5d68baa
Tsunami014 (Max) <Tsunami014@duck.com>
2025-01-25 12:02:05 +1100

    Moved the IO to a file called IO.py and implemented a TerminalAPI and a Window and TextWindow

---
+++ IO.py
@@ -0,0 +1,78 @@
+import select
+from queue import Queue, Empty
+from threading import Thread
+
+class Pollable:
+    def __init__(self, file):
+        self.f = file
+        self.fd = b''
+        self.poll = select.poll()
+        self.poll.register(file, select.POLLIN)
+        self.readMoreData()
+    
+    def readMoreData(self):
+        while self.poll.poll(1):
+            self.fd += self.f.read(1)
+
+    def read(self, size):
+        if len(self.fd) < size:
+            self.readMoreData()
+            if len(self.fd) < size:
+                return None
+        data = self.fd[:size]
+        self.fd = self.fd[size:]
+        return data
+    
+    def __getitem__(self, idx):
+        assert isinstance(idx, int)
+        if len(self.fd) < idx:
+            self.readMoreData()
+            if len(self.fd) < idx:
+                return False
+        return True
+    
+    def __bool__(self):
+        if not self.fd:
+            self.readMoreData()
+        return bool(self.fd)
+
+class Readable:
+    def __init__(self, file):
+        self.f = file
+        self.fd = ''
+        self.q = Queue()
+        t = Thread(target=self.enqueue_output, args=(self.f, self.q), daemon=True)
+        t.start()
+        self.readMoreData()
+    
+    @staticmethod
+    def enqueue_output(out, queue):
+        while True:
+            queue.put(out.read(1))
+    
+    def readMoreData(self):
+        try:
+            line = self.q.get_nowait()
+            self.fd += line
+        except Empty:
+            pass
+
+    def read(self, size):
+        if not self:
+            return ''
+        data = self.fd[:size]
+        self.fd = self.fd[size:]
+        return data
+    
+    def __getitem__(self, idx):
+        assert isinstance(idx, int)
+        if len(self.fd) < idx:
+            self.readMoreData()
+            if len(self.fd) < idx:
+                return False
+        return True
+    
+    def __bool__(self):
+        if not self.fd:
+            self.readMoreData()
+        return bool(self.fd)
--- OS.py
+++ OS.py
@@ -3,147 +3,147 @@ import os
 import sys
 import termios
 import tty
-import select
-from queue import Queue, Empty
-from threading import Thread
+from IO import Pollable, Readable
 
-def get_terminal_size():
-    rows, cols = os.popen('stty size', 'r').read().split()
-    return int(rows), int(cols)
+class TerminalAPI:
+    def __init__(self):
+        self._windows = []
+        self.MOUSE = [0, 0]
+        self.MOUSE_STATUS = 0
+        self.MOUSE_SENSITIVITY = [0.249, 0.13]
+    
+    def add_window(self, window):
+        self._windows.append(window)
+    
+    def remove_window(self, window):
+        self._windows.remove(window)
+    
+    def drawAll(self):
+        for window in self._windows:
+            window.draw()
+    
+    @property
+    def moveToMouse(self):
+        return f'\033[{round(API.MOUSE[1])};{round(API.MOUSE[0])}H'
+    
+    @staticmethod
+    def get_terminal_size():
+        rows, cols = os.popen('stty size', 'r').read().split()
+        return int(rows), int(cols)
 
-def draw_border(rows, cols):
+API = TerminalAPI()
+
+class Window:
+    API = API
+    def __new__(cls, *args, **kwargs):
+        window = super().__new__(cls)
+        cls.API.add_window(window)
+        return window
+    
+    def __del__(self):
+        self.API.remove_window(self)
+    
+    def draw(self):
+        pass
+
+    def __str__(self):
+        return '<Window object>'
+    def __repr__(self): return str(self)
+
+class TextWindow(Window):
+    def __init__(self, x, y, text, max_width=None):
+        self.x = x
+        self.y = y
+        self.max_width = max_width
+        self.text = text
+    
+    def draw(self):
+        if self.max_width:
+            lines = []
+            for paragraph in self.text.split('\n'):
+                while len(paragraph) > self.max_width:
+                    space_index = paragraph.rfind(' ', 0, self.max_width)
+                    if space_index == -1:
+                        space_index = self.max_width
+                    lines.append(paragraph[:space_index])
+                    paragraph = paragraph[space_index:].lstrip()
+                lines.append(paragraph)
+        else:
+            lines = self.text.split('\n')
+        width, height = max(len(i) for i in lines), len(lines)
+        sys.stdout.write(f'\033[{self.y};{self.x}H╭'+('─'*width)+'╮')
+        for idx, ln in enumerate(lines):
+            sys.stdout.write(f'\033[{self.y+idx+1};{self.x}H│{ln}│')
+        sys.stdout.write(f'\033[{self.y+height+1};{self.x}H╰'+('─'*width)+'╯')
+
+WIND = TextWindow(10, 20, 'Hello, World!')
+
+def draw_border():
+    rows, cols = API.get_terminal_size()
     sys.stdout.write('\033[H')  # Move cursor to top-left
     sys.stdout.write('╭'+'─' * (cols-2)+'╮')  # Top border
     for row in range(2, rows):
         sys.stdout.write(f'\033[{row};1H│\033[{row};{cols}H│')  # Side borders
     sys.stdout.write(f'\033[{rows};1H╰' + '─' * (cols-2)+'╯')  # Bottom border
-    sys.stdout.flush()
 
 def clear_console():
     sys.stdout.write('\033[2J\033[0;0H')
-    sys.stdout.flush()
-
-class Pollable:
-    def __init__(self, file):
-        self.f = file
-        self.fd = b''
-        self.poll = select.poll()
-        self.poll.register(file, select.POLLIN)
-        self.readMoreData()
-    
-    def readMoreData(self):
-        while self.poll.poll(1):
-            self.fd += self.f.read(1)
-
-    def read(self, size):
-        if len(self.fd) < size:
-            self.readMoreData()
-            if len(self.fd) < size:
-                return None
-        data = self.fd[:size]
-        self.fd = self.fd[size:]
-        return data
-    
-    def __getitem__(self, idx):
-        assert isinstance(idx, int)
-        if len(self.fd) < idx:
-            self.readMoreData()
-            if len(self.fd) < idx:
-                return False
-        return True
-    
-    def __bool__(self):
-        if not self.fd:
-            self.readMoreData()
-        return bool(self.fd)
-
-class Readable:
-    def __init__(self, file):
-        self.f = file
-        self.fd = ''
-        self.q = Queue()
-        t = Thread(target=self.enqueue_output, args=(self.f, self.q), daemon=True)
-        t.start()
-        self.readMoreData()
-    
-    @staticmethod
-    def enqueue_output(out, queue):
-        while True:
-            queue.put(out.read(1))
-    
-    def readMoreData(self):
-        try:
-            line = self.q.get_nowait()
-            self.fd += line
-        except Empty:
-            pass
-
-    def read(self, size):
-        if not self:
-            return ''
-        data = self.fd[:size]
-        self.fd = self.fd[size:]
-        return data
-    
-    def __getitem__(self, idx):
-        assert isinstance(idx, int)
-        if len(self.fd) < idx:
-            self.readMoreData()
-            if len(self.fd) < idx:
-                return False
-        return True
-    
-    def __bool__(self):
-        if not self.fd:
-            self.readMoreData()
-        return bool(self.fd)
 
 def main():
     clear_console()
-    rows, cols = get_terminal_size()
-    draw_border(rows, cols)
+    draw_border()
+    API.drawAll()
 
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     tty.setraw(fd)
-
-    MOUSE = [0, 0]
-    MOUSE_SENSITIVITY = [0.249, 0.13]
     run = True
+
+    API.MOUSE = [API.get_terminal_size()[1]//2, API.get_terminal_size()[0]//2]
 
     try:
         with open('/dev/input/mice', 'rb') as file:
             mouse = Pollable(file)
             stdin = Readable(sys.stdin)
             while run:
+                update = False
+                API.MOUSE_STATUS = 0
                 while mouse[3]:
+                    update = True
                     data = mouse.read(3)
                     status, dx, dy = data[0], data[1], data[2]
                     if dx > 127:
                         dx -= 256
                     if dy > 127:
                         dy -= 256
-                    MOUSE[0] += dx*MOUSE_SENSITIVITY[0]
-                    MOUSE[1] -= dy*MOUSE_SENSITIVITY[1]
+                    API.MOUSE[0] += dx*API.MOUSE_SENSITIVITY[0]
+                    API.MOUSE[1] -= dy*API.MOUSE_SENSITIVITY[1]
 
-                    tSize = get_terminal_size()
-                    MOUSE[0] = max(1, min(tSize[1], MOUSE[0]))
-                    MOUSE[1] = max(1, min(tSize[0], MOUSE[1]))
+                    tSize = API.get_terminal_size()
+                    API.MOUSE[0] = max(2, min(tSize[1]-1, API.MOUSE[0]))
+                    API.MOUSE[1] = max(2, min(tSize[0]-1, API.MOUSE[1]))
 
-                    moveTo = f'\033[{round(MOUSE[1])};{round(MOUSE[0])}H'
-                    sys.stdout.write(moveTo)
-                    if status & 1:  # Left button clicked
-                        sys.stdout.write(','+moveTo)
+                    API.MOUSE_STATUS = status
+
+                if API.MOUSE_STATUS & 1:  # Left button clicked
+                    sys.stdout.write(API.moveToMouse+',')
                 
                 while stdin:
                     char = stdin.read(1)
                     if char == '\x03' or char == '\x1b':  # Ctrl+C or ESC
                         run = False
                 
+                if update:
+                    #clear_console()
+                    draw_border()
+                    API.drawAll()
+                
+                sys.stdout.write(API.moveToMouse)
                 sys.stdout.flush()
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         clear_console()
+        sys.stdout.flush()
 
 if __name__ == '__main__':
     main()
